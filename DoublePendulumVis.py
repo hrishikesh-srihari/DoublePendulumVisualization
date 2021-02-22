95560f0b42880f0a4330b20b4918d3317d6e139f
Hrishikesh Srihari <rishisrihari9@gmail.com>
2021-02-22 15:03:54 -0600

    added imports and main values

--- DoublePendulumVis.py
+++ DoublePendulumVis.py
@@ -1,1 +1,11 @@
+from numpy import sin, cos
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.integrate as integrate
+import matplotlib.animation as animation
 
+g = 9.8
+l1 = 1.0
+l2 = 1.0
+m1 = 1.0
+m2 = 1.0
