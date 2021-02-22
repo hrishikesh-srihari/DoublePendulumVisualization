00f2aca633f66eae9532c857638c9cadc4088d76
Hrishikesh Srihari <rishisrihari9@gmail.com>
2021-02-22 16:37:31 -0600

    initialized window and animated motion

--- DoublePendulumVis.py
+++ DoublePendulumVis.py
@@ -39,8 +39,36 @@ w2 = 0.0
 state = np.radians([th1, w1, th2, w2])
 y = integrate.odeint(derivs, state, t)
 
-x1 = L1*sin(y[:, 0])
-y1 = -L1*cos(y[:, 0])
+x1 = l1*sin(y[:, 0])
+y1 = -l1*cos(y[:, 0])
 
-x2 = L2*sin(y[:, 2]) + x1
-y2 = -L2*cos(y[:, 2]) + y1
+x2 = l2*sin(y[:, 2]) + x1
+y2 = -l2*cos(y[:, 2]) + y1
+
+fig = plt.figure()
+ax = fig.add_subplot(111, autoscale_on=False, xlim=(-2, 2), ylim=(-2, 2))
+ax.set_aspect('equal')
+ax.grid()
+
+line, = ax.plot([], [], 'o-', lw=2)
+time_template = 'time = %.1fs'
+time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+
+
+def init():
+    line.set_data([], [])
+    time_text.set_text('')
+    return line, time_text
+
+def animate(i):
+    thisx = [0, x1[i], x2[i]]
+    thisy = [0, y1[i], y2[i]]
+
+    line.set_data(thisx, thisy)
+    time_text.set_text(time_template % (i*dt))
+    return line, time_text
+
+
+ani = animation.FuncAnimation(fig, animate, range(1, len(y)),
+                              interval=dt*1000, blit=True, init_func=init)
+plt.show()
